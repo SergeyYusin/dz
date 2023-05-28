543572d4d7e5ac08481621dbf3526dfdc87a40be
unknown <cruspe123@mail.ru>
2023-05-28 23:35:34 +0300

    qwt

---
+++ Example010/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+int[] array = { 15, 28, 37, 44, 56, 56, 473, 82, 91 };
+
+int n = array.Length;
+int find = 56;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index ++;
+}
